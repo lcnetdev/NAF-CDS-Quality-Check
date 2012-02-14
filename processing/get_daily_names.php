0b45a69f7d67754ec7fe4a46f9115f03a8e01c76
Kevin Ford <kefo@loc.gov>
2012-02-14 17:24:14 -0500

    Added timezone so PHP5 compliant. Added ftp_pasv because failed to establish reliable connection to ftp server.

--- processing/get_daily_names.php
+++ processing/get_daily_names.php
@@ -1,5 +1,7 @@
 <?php
 
+date_default_timezone_set('America/New_York');
+
 require_once("ftp_creds.php");
 
 $lastDownload = trim(file_get_contents("last_daily_name_retrieved.txt"));
@@ -7,16 +9,20 @@ $lastDownload = trim(file_get_contents("last_daily_name_retrieved.txt"));
 $nextday = strtotime($lastDownload . " +1 day");
 
 $lastDownloadFormatted = "d" . date("ymd", $nextday) . ".records.xml";
-echo $lastDownloadFormatted."\n";
 
 $localfile = LOCALDAYNAMEDIR . $lastDownloadFormatted;
 $serverfile = SERVERDAYNAMEDIR . $lastDownloadFormatted;
 
 // set up basic connection
-$conn_id = ftp_connect(FTPSERVER);
+$conn_id = ftp_connect(FTPSERVER) or die("Failed to connect to " . FTPSERVER);
 
 // login with username and password
-$login_result = ftp_login($conn_id, FTPUSER, FTPPASS);
+$login_result = ftp_login($conn_id, FTPUSER, FTPPASS) or die("Failed to login to " . FTPSERVER . " as " . FTPUSER);
+
+// turn passive mode on
+ftp_pasv($conn_id, true);
+
+echo "Retrieving $lastDownloadFormatted \n";
 
 // try to download $server_file and save to $local_file
 if (ftp_get($conn_id, $localfile, $serverfile, FTP_BINARY)) {
